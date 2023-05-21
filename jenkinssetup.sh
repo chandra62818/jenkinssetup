431e2656511f3b22ad0c77a8e77c204d1f4ed13e
root <root@ip-172-31-39-94.ap-south-1.compute.internal>
2023-05-21 06:51:42 +0000

    git setup

---
+++ jenkinssetup.sh
@@ -0,0 +1,13 @@
+ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+
+ amazon-linux-extras install java-openjdk11 -y
+
+
+ yum install git maven jenkins -y
+
+ systemctl restart jenkins.service
+
+ systemctl status jenkins.service
+
